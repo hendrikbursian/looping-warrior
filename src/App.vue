f7119518251fc89942d2b04b4a9351db04d87c69
Hendrik Bursian <hendrikbursian@protonmail.com>
2020-11-22 03:57:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav class="bg-gray-500 px-4">
+      <ul class="flex justify-end text-green-400">
+        <li class="flex">
+          <router-link
+            exact-active-class="bg-green-700"
+            class="bg-gray-700 py-8 px-10 hover:bg-green-700"
+            to="/"
+          >
+            Home
+          </router-link>
+        </li>
+        <li class="flex">
+          <router-link
+            exact-active-class="bg-green-700"
+            class="bg-gray-700 py-8 px-10 hover:bg-green-700"
+            to="/music"
+          >
+            Music
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
